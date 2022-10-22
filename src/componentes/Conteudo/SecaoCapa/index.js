92f3bca8967ba3aa08e0974e7d48854f294ea6f1
JhonatanSO <jhonerex2012@gmail.com>
2022-10-22 10:13:37 -0300

    adc estilo da secao capa

--- src/componentes/Conteudo/SecaoCapa/index.js
+++ src/componentes/Conteudo/SecaoCapa/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './style.css';
 
 export default function SecaoCapa() {
   return (
-    <section>
-      <div>
+    <section className="secao-capa">
+      <div className="limitar-secao secao-capa-container">
         <p>Preços baixos em</p>
         <h1>ÓCULOS DE GRAU E DE SOL</h1>
         <p>Você só encontra aqui</p>
