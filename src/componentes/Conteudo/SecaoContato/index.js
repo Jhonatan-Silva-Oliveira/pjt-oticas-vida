c215767bd757f2d9f526dae4b9a7bab126b82018
JhonatanSO <jhonerex2012@gmail.com>
2022-10-23 10:05:28 -0300

    adc estilo secao contato

--- src/componentes/Conteudo/SecaoContato/index.js
+++ src/componentes/Conteudo/SecaoContato/index.js
@@ -1,38 +1,39 @@
 import React from 'react';
+import './style.css'
 
 export default function SecaoContato() {
   return (
-    <section>
-      <div id="Contato">
+    <section className="secao-contato">
+      <div className="limitar-secao" id="Contato">
         <h2>FALE CONOSCO</h2>
         <p>Não perca tempo, venha conhecer uma de nossas lojas ou entre contato através de nossas redes sociais ou da central de atendimento.</p>
-        <div>
-          <div className="contatos">
+        <div className="container-contatos">
+          <div className="container-box-contatos">
             <h3>Contato</h3>
-            <div>
+            <div className="box-contatos">
               <img src="./assets/imagens/local.png"></img>
               <p>Nova Iguaçu, RJ</p>
             </div>
-            <div>
+            <div className="box-contatos">
               <img src="./assets/imagens/telefone.png"></img>
               <p>(21) 99999-9999</p>
             </div>
-            <div>
+            <div className="box-contatos">
               <img src="./assets/imagens/email.png"></img>
               <p>contato@oticavida.com</p>
             </div>
           </div>
-          <div className="redes-sociais">
+          <div className="container-box-contatos">
             <h3>Nossas Redes Sociais</h3>
-            <div>
+            <div className="box-contatos">
               <img src="./assets/imagens/fb.png"></img>
               <p>/OticaVida</p>
             </div>
-            <div>
+            <div className="box-contatos">
               <img src="./assets/imagens/ig.png"></img>
               <p>@oticavidarj</p>
             </div>
-            <div>
+            <div className="box-contatos">
               <img src="./assets/imagens/tt.png"></img>
               <p>@oticavidarj</p>
             </div>
