813e9e2a8f017dc0d14f639196aad3ee88f75b3a
JhonatanSO <jhonerex2012@gmail.com>
2022-10-23 09:14:00 -0300

    alt nome class em secao produtos, adc estilo secao sobre

--- src/componentes/Conteudo/SecaoProdutos/index.js
+++ src/componentes/Conteudo/SecaoProdutos/index.js
@@ -9,23 +9,23 @@ export default function SecaoProdutos() {
         <p>Tralhamos com óculos de grau, óculos de sol, lentes transition nos modelos masculinos, femininos e infantil.</p>
         <p>Todos os nossos preços são acessíveis e contam com a melhor qualidade do mercado.</p>
         
-        <div className="cards-container">
-          <div className="card">
+        <div className="cards-container-produtos">
+          <div className="card-produtos">
             <h3>Óculos de grau</h3>
             <img src="../assets/imagens/oculos01.png"></img>
             <p>R$ 500,00</p>
           </div>
-          <div className="card">
+          <div className="card-produtos">
             <h3>Óculos de grau</h3>
             <img src="../assets/imagens/oculos02.png"></img>
             <p>R$ 500,00</p>
           </div>
-          <div className="card">
+          <div className="card-produtos">
             <h3>Óculos de grau</h3>
             <img src="../assets/imagens/oculos03.png"></img>
             <p>R$ 500,00</p>
           </div>
-          <div className="card">
+          <div className="card-produtos">
             <h3>Óculos de grau</h3>
             <img src="../assets/imagens/oculos04.png"></img>
             <p>R$ 500,00</p>
--- src/componentes/Conteudo/SecaoSobre/index.js
+++ src/componentes/Conteudo/SecaoSobre/index.js
@@ -1,25 +1,26 @@
 import React from 'react';
+import './style.css'
 
 export default function SecaoSobre() {
   return (
-    <section>
-      <div id="Sobre">
+    <section className="secao-sobre">
+      <div className="limitar-secao" id="Sobre">
         <h2>QUEM SOMOS NÓS?</h2>
         <p>Fundada em 2001, em Nova Iguaçu - Rio de Janeiro, a Óticas vida iniciou suas atividades focada np atendimento ao público de renda mais baixa, sempre com o objetivo de proporcionar ao cliente bom atendimento, qualidade e preço baixo.</p>
-        <div>
-          <div>
+        <div className="cards-container-sobre">
+          <div className="card-sobre">
             <img src="./assets/imagens/loja.png"></img>
           </div>
-          <div>
+          <div className="card-sobre card-text">
             <h3>NOSSAS FILIAIS</h3>
             <p>Hoje temos mais de 20 filiais pelo Brasil e na América</p>
           </div>
-          <div>
-            <img src="./assets/imagens/atendimento.png"></img>
-          </div>
-          <div>
+          <div className="card-sobre card-text">
             <h3>ATENDIMENTO FLEXÍVEL</h3>
             <p>Nossa equipe especializada é treinada para te atender</p>
+          </div>
+          <div className="card-sobre">
+            <img src="./assets/imagens/atendimento.png"></img>
           </div>
         </div>
       </div>
